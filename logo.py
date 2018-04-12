63d739ce9d4db6da5685c81d82d2a522c80c7c0a
AHMED ABDULWAHAB <s338486830@ocdsb>
2018-04-12 11:02:46 -0400

    retro IG logo. Almost done. Need to smooth edges with arc

--- logo.py
+++ logo.py
@@ -6,14 +6,23 @@
 
 # Import the pygame library and initialise the game engine
 import pygame
+import math  
 pygame.init()
 
 # Define some colours
 # Colours are defined using RGB values
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
-GREEN = (0, 255, 0)
+GWEEN = (129, 210, 169)
 RED = (255, 0, 0)
+BROWN = (106, 69, 60)
+PALE = (231, 221, 209)
+BLUE = (71, 155, 251)
+DARK = (61, 58, 53) 
+MIDBLACK = (52, 52, 52) 
+YELLOW = (255, 207, 58) 
+WED = (253, 59, 96)
+GRAY = (182, 172, 170)
 
 # Set the screen size (please don't change this)
 SCREENWIDTH = 400
@@ -42,11 +51,22 @@ while carryOn:
     # There should be none for a static image
     
     # --- Draw code goes here
-
+    
     # Clear the screen to white
     screen.fill(WHITE)
 
     # Queue different shapes and lines to be drawn
+    pygame.draw.rect(screen, PALE, [10, 80, 380, 380], 0)
+    pygame.draw.rect(screen, BROWN, [10, 10, 380, 100], 0)
+    pygame.draw.rect(screen, BLACK, [340, 40, 40,40],0)
+    pygame.draw.rect(screen, BLUE, [100, 20, 20, 90], 0)
+    pygame.draw.rect(screen, GWEEN, [80, 20, 20, 90], 0)
+    pygame.draw.rect(screen, YELLOW, [60, 20, 20, 90],0)
+    pygame.draw.rect(screen, WED, [40, 20,20,90], 0)
+    pygame.draw.rect(screen, MIDBLACK, [40, 100, 80, 30] , 0)
+    pygame.draw.ellipse(screen, MIDBLACK, [130, 150, 150, 150],0)
+    pygame.draw.ellipse(screen, BLACK,[125, 145, 160,160], 20)
+    pygame.draw.ellipse(screen, GRAY,[115, 135, 180,180], 15)
     # pygame.draw.rect(screen, RED, [55, 200, 100, 70], 0)
     # pygame.draw.line(screen, GREEN, [0, 0], [100, 100], 5)
     # pygame.draw.ellipse(screen, BLACK, [20, 20, 250, 100], 2)
